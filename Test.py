04a4c2875bbd9e8ce1496650405c14bf5d58b573
zpj <550392179@qq.com>
2020-05-03 20:56:13 +0800

    配置TF

---
+++ Test.py
@@ -0,0 +1,31 @@
+'''
+@Author: your name
+@Date: 2019-12-22 22:48:14
+@LastEditTime: 2020-05-03 20:36:23
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \Tensorflow\Test\Test.py
+'''
+import tensorflow as tf 
+
+
+# import tensorflow as tf
+# import cv2
+
+
+print(tf.__version__)
+
+
+
+# a = tf.constant([[10,10],[11.,1.]])
+# x = tf.constant([[1.,0.],[0.,1.]])
+# b = tf.Variable(12.)
+# y = tf.matmul(a, x) + b
+# print(y.numpy())
+
+# img=cv2.imread('D:\\Tensorflow\\Test\\lufei.jpg')
+# cv2.imshow('img',img)
+# cv2.waitKey(0)
+
+
+
